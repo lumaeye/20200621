4cdb63b930d5f205e03b4cf58f738eaa5f3e67dd
lumaeye <lumaeye@gmail.com>
2020-06-21 16:17:06 +0800

    20200621

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('#banner').slick({
+    autoplay:true,
+    autoplaySpeed: 1000,
+    arrows:false
+
+});
+$('#contact').slick({
+    slidesToShow:3,
+    autoplay:true,
+    autoplaySpeed: 1000,
+    dots:true,
+    arrows:false
+    
+});
